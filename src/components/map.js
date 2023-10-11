124db2eb4a7f9f8c22162af57cbe86aff6fe171c
Chamsron <emmacameron219@gmail.com>
2023-10-11 19:59:29 +0100

    Can select dojo and map re-renders

---
+++ src/components/map.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { loadModules } from 'esri-loader';
+
+const EmmaMap = () => {
+  const mapRef = useRef(null);
+
+  let centerCoordinates = [-4.2518, 55.8642];
+  const [selectedDojo, setSelectedDojo] = useState(centerCoordinates);
+
+  useEffect(() => {
+    loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
+      .then(([Map, MapView]) => {
+        const map = new Map({
+          basemap: 'streets-navigation-vector'
+        });
+
+        const view = new MapView({
+          container: mapRef.current,
+          map: map,
+          center: centerCoordinates,
+          zoom: 15
+        });
+
+        view.when(() => {});
+      })
+      .catch((err) => console.error(err));
+  }, [selectedDojo]);
+
+  const handleDojoChange = (event) => {
+    setSelectedDojo(event.target.value);
+  };
+
+  if (selectedDojo === 'TaiSeiDoKai'){
+    centerCoordinates = [-4.262670, 55.869510]
+  } else if (selectedDojo === 'EdinburghCity'){
+    centerCoordinates = [-3.220820, 55.938160]
+  } else if (selectedDojo === 'AberdeenCity'){
+    centerCoordinates = [-2.190740, 57.116402]
+  }
+
+  useEffect(() => {
+    if (mapRef.current && mapRef.current.view){
+        const view = mapRef.current.view;
+        view.center = centerCoordinates;
+    }
+  }, [centerCoordinates]);
+
+  return (
+    <div>
+      <select value={selectedDojo} onChange={handleDojoChange}>
+        <option value="TaiSeiDoKai">TaiSeiDoKai</option>
+        <option value="EdinburghCity">Edinburgh City</option>
+        <option value="AberdeenCity">Aberdeen City</option>
+      </select>
+      <div ref={mapRef} style={{ height: '50vh', width: '100%' }} />
+    </div>
+  );
+};
+
+export default EmmaMap;
+
